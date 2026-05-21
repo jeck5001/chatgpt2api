9b6f10861d72aa4f89bf9b78f2fc0fdc3fb202e4
qingfeng <qingfeng@local.dev>
2026-05-21 02:19:34 +0000

    test(app): stabilize startup screen test

--- apps/image_studio_app/test/widget_test.dart
+++ apps/image_studio_app/test/widget_test.dart
@@ -1,8 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:image_studio_app/app/image_studio_app.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   testWidgets('app starts at onboarding screen', (tester) async {
+    SharedPreferences.setMockInitialValues(<String, Object>{});
+
     await tester.pumpWidget(const ImageStudioApp());
     await tester.pumpAndSettle();
 
